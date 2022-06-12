f0b3894e3b9708449be2eb54b909db8a21ca54fe
Kundayic <kundyc97@gmail.com>
2022-06-12 00:27:50 -0700

    the demo

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- *
- *  * more_numbers - prints more numbers
+ * more_numbers - prints more numbers
  */
 
 void more_numbers(void)
 {
 	int i, j;
-	
+
 	i = 0;
-	
+
 	while (i < 10)
 	{
 		for (j = 0; j < 15; j++)
