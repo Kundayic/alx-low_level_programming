d735f28a256126214d4ea7115b179667361bb3d6
Kundayic <kundyc97@gmail.com>
2022-06-13 06:04:23 -0700

    the demo

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,29 +1,11 @@
 #include "main.h"
+
 /**
- * _strcat - concatenates two strings
- * @dest: A pointer to a character that will be changed
- * @src: A pointer to a character that will also be changed
- * Return: dest
+ * reset_to_98 - reverts poiter to 98
+ * @n: pointer
  */
 
-char *_strcat(char *dest, char *src)
+void reset_to_98(int *n)
 {
-int i, j;
-
-i = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
-
-j = 0;
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-j++;
-i++;
-}
-dest[i] = '\0';
-
-return (dest);
+	*n = 98;
 }
